60ad0484a17771cc22a2fb2f2e6be2f241371aed
nataliepiehl <piehl.natalie@gmail.com>
2022-04-11 21:03:40 -0500

    started html and js code

---
+++ data.js
@@ -0,0 +1,28 @@
+// Define local URL
+var url = "http://127.0.0.1:5000/";
+
+// Define query
+var query = "api/movie_title/";
+
+// Define title of interest
+var title = "the&mummy";
+
+// Define year of interest
+// var year = 1955
+
+// Define full query
+let full_query = url + query + title;
+document.write(full_query);
+
+// Fetch the data
+fetch(full_query)
+    .then(response => {
+        response => response.json()
+    })
+    .then((data) => {
+        let movie = data
+    })
+
+document.write(movie);
+// let response = fetch(full_query);
+
