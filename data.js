1c1f838d2a7a2175f082554f57f843fcb8a75bba
Jeff Overholt <jeffrey.a.overholt@usps.gov>
2022-04-14 08:43:55 -0400

    Working code for HTML and JS File

--- data.js
+++ data.js
@@ -29,3 +29,16 @@ fetch(full_query)
 
 // let response = fetch(full_query);
 
+function buildMovie(movieTitle) {
+    d3.json("movie").then((data) => {
+        var movieName = data.0;
+        var resultArray = 0.filter(x => x.0 == movieTitle);
+        var result = resultArray[0];
+        console.log(result)
+        var PANEL = d3.select("movieTitle-0");
+        PANEL.html("");
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        })
+    })
+}
