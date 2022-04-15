9a261db72c585d5e2daee8129379dec28b7b5de0
Jeff Overholt <jeffrey.a.overholt@usps.gov>
2022-04-15 17:41:24 -0400

    Working code

--- data.js
+++ data.js
@@ -27,18 +27,18 @@ fetch(full_query)
         console.log(data)
     })
 
-let response = fetch(full_query);
+// let response = fetch(full_query);
 
-function buildMovie(movieTitle) {
-    d3.json("movie").then((data) => {
-        var movieName = data.0;
-        var resultArray = 0.filter(x => x.0 == movieTitle);
-        var result = resultArray[0];
-        console.log(result)
-        var PANEL = d3.select("movieTitle-0");
-        PANEL.html("");
-        Object.entries(result).forEach(([key, value]) => {
-            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-        })
-    })
-}
+// function buildMovie(movieTitle) {
+//     d3.json("movie").then((data) => {
+//         var movieName = data.0;
+//         var resultArray = 0.filter(x => x.0 == movieTitle);
+//         var result = resultArray[0];
+//         console.log(result)
+//         var PANEL = d3.select("movieTitle-0");
+//         PANEL.html("");
+//         Object.entries(result).forEach(([key, value]) => {
+//             PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+//         })
+//     })
+// }
