0a2eb42ff526c4c88954af0b13c233a8f03f6bd9
Jeff Overholt <jeffrey.a.overholt@usps.gov>
2022-04-18 22:07:36 -0400

    updated JS

--- data.js
+++ data.js
@@ -11,17 +11,50 @@ var full_query = url + query;
 console.log(full_query)
 
 // Fetch the data
-fetch(full_query)
-    .then(response =>
-        response.json()
-    )
-    .then((data) => {
-        console.log(data)
-    })
+// fetch(full_query)
+//     .then(response =>
+//         response.json()
+//     )
+//     .then((data) => {
+//         console.log(data)
+//     })
+
+// var ratings = data.ratings.title
+//     console.log(ratings)
+
+
+// init(data)
 
 // let response = fetch(full_query);
 
-// function buildMovie(movieTitle) {
+// function init() {
+    d3.json(full_query).then(data => {
+        console.log(full_query);
+        console.log(data);
+        var names = data.ratings.title;
+        let dropdownMenu = d3.select("#selDataset");
+        // data.ratings.slice(0, 1000).forEach((name) => {
+        data.ratings.forEach((name) => {
+            dropdownMenu.append('option').text(name.title);
+          })
+
+          var result = names[0];
+
+    buildMetadata(result)
+    charts(result)
+  });
+// }
+
+function optionChanged(value) {
+  console.log(value);
+  charts(value);
+  buildMetadata(value);
+}
+
+// init();
+
+
+// function buildRatings(movieTitle) {
 //     d3.json("movie").then((data) => {
 //         var movieName = data.0;
 //         var resultArray = 0.filter(x => x.0 == movieTitle);
